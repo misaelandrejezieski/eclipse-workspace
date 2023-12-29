3b5d9271f7c5429b6899654afec4c4647d14c6b6
Misael Andrejezieski <misaelandrejezieski130982@outlook.com>
2023-12-29 18:33:39 -0300

    Update Test_Class_001.java

--- Test_002/src/Test_Package_001/Test_Class_001.java
+++ Test_002/src/Test_Package_001/Test_Class_001.java
@@ -1,8 +1,8 @@
 package Test_Package_001;
 
-import java.awt.Panel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
